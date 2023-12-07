3e4618f2a83f6ccc088624699601e7bbf280045c
LordSir101 <tywunderlich15@gmail.com>
2023-12-06 22:10:00 -0500

    day 6

--- 6/part1/solution.cpp
+++ 6/part1/solution.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include<vector>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>  
 using namespace std;
 
 int main () {
@@ -10,17 +14,47 @@ int main () {
   if (myfile.is_open())
   {
 
-    Vector<string> time;
-    Vector<string> records;
+    vector<int> times;
+    vector<int> records;
+    int line_num = 1;
     while(getline(myfile, line)) {
-      char * ;
-      pch = strtok(str," ,.-");
-      while (pch != NULL)
-      {
-        printf ("%s\n",pch);
-        pch = strtok (NULL, " ,.-");
+      std::string token = line.substr(line.find(":") + 1, line.size());
+      //cout << token << endl;
+      std::istringstream iss{ token};
+      int number;
+      if(line_num == 1){
+          while(iss >> number){
+              times.push_back(number);
+          }
       }
+      else{
+           while(iss >> number){
+              records.push_back(number);
+          }
+      }
+        
+        line_num++;
     }
+    
+    // use quadratic formula to fin roots when y = record + 1
+    int total = 1;
+    for(int i = 0; i < times.size(); i++){
+        int a = -1;
+        int b = times[i];
+        int c = (records[i]+1) * -1;
+        
+        float dis = sqrt(pow(b,2) - (4*a*c));
+        
+        float r1 = (-1*b+ dis) / 2*a;
+        float r2 = (-1*b - dis) / 2*a;
+        
+        int lowest = ceil(min(r1,r2));
+        int highest = floor(max(r1,r2));
+        
+        int diff = highest - lowest;
+        total *= diff + 1;
+    }
+    cout << total << endl;
     myfile.close();
   }
 
@@ -28,3 +62,4 @@ int main () {
 
   return 0;
 }
+    
--- 6/part2/solution.cpp
+++ 6/part2/solution.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include<vector>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
 using namespace std;
 
 int main () {
@@ -10,17 +15,39 @@ int main () {
   if (myfile.is_open())
   {
 
-    Vector<string> time;
-    Vector<string> records;
+    string times;
+    string records;
+    int line_num = 1;
     while(getline(myfile, line)) {
-      char * ;
-      pch = strtok(str," ,.-");
-      while (pch != NULL)
-      {
-        printf ("%s\n",pch);
-        pch = strtok (NULL, " ,.-");
+      std::string token = line.substr(line.find(":") + 1, line.size());
+      std::string::iterator end_pos = std::remove(token.begin(), token.end(), ' ');
+      token.erase(end_pos, token.end());
+      int number;
+      if(line_num == 1){
+        times = token;
       }
+      else{
+        records = token;
+      }
+        
+        line_num++;
     }
+    
+    int a = -1;
+    long b = stol(times);
+    long c = (stol(records)+1) * -1;
+    
+    double dis = sqrt(pow(b,2) - (4*a*c));
+    
+    double r1 = (-1*b+ dis) / 2*a;
+    double r2 = (-1*b - dis) / 2*a;
+    
+    long lowest = ceil(min(r1,r2));
+    long highest = floor(max(r1,r2));
+    
+    long total = highest - lowest + 1;
+    
+    cout << total << endl;
     myfile.close();
   }
 
@@ -28,3 +55,4 @@ int main () {
 
   return 0;
 }
+    
